90b832edfa9bcc22544a809774a29289dc9f30b3
Lydia Tan <lydia.sydia@gmail.com>
2018-07-12 13:14:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  var newArray = drivers.slice();
+  newArray.push("Broom");
+  return newArray;
+}
+
+function prependDriver(name){
+  var newerArray = drivers.slice();
+  newerArray.unshift("Bob");
+  return newerArray;
+}
